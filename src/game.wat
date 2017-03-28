f10d0e2fa85a1add6909a91ec92e9155c95c6b0e
ABA Games <cs8k-cyu@asahi-net.or.jp>
2017-03-28 22:59:52 +0900

    Add a vram and a keymap

--- src/game.wat
+++ src/game.wat
@@ -1,3 +1,9 @@
 (module
-  (func (export "update"))
+  (memory (export "ram") 1)
+  (func (export "update")
+    (if (i32.load8_u (i32.const 293))
+      (then (i32.store8 (i32.const 0) (i32.const 1)))
+      (else (i32.store8 (i32.const 0) (i32.const 0)))
+    )
+  )
 )
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
 import * as game from './game.wat';
 
 declare const WebAssembly: any;
+let context: CanvasRenderingContext2D;
+let ram: Uint8Array;
 
 window.onload = init;
 
 function init() {
   instantiate(game, {}).then(instance => {
+    new WebAssembly.Memory({ initial: 512 / 4 });
+    ram = new Uint8Array(instance.exports.ram.buffer);
+    document.onkeydown = e => {
+      ram[e.keyCode + 256] = 1;
+    };
+    document.onkeyup = e => {
+      ram[e.keyCode + 256] = 0;
+    };
+    const canvas = <HTMLCanvasElement>document.getElementById('main');
+    context = canvas.getContext('2d');
+    context.fillStyle = 'white';
     update(instance);
   });
 }
 
 function update(instance) {
   instance.exports.update();
+  context.clearRect(0, 0, 16, 16);
+  for (let i = 0; i < 256; i++) {
+    const x = i % 16;
+    const y = Math.floor(i / 16);
+    if (ram[i] > 0) {
+      context.fillRect(x, y, 1, 1);
+    }
+  }
   requestAnimationFrame(() => update(instance));
 }
 
